15d23d8d5db936e4cf757d9f4191ae322a078fcc
Dharshini <dharshu@example.com>
2025-10-09 11:26:06 +0530

    signin

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,20 +1,25 @@
 import API from "./api";
 
 export const AuthService = {
+  // Login using form-urlencoded
   signin: (data: { username: string; password: string }) => {
-    // Form-urlencoded data
     const formData = new URLSearchParams();
     formData.append("username", data.username);
     formData.append("password", data.password);
 
     return API.post("/login", formData, {
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded", 
+        "Content-Type": "application/x-www-form-urlencoded",
       },
     });
   },
 
+  // Signup uses JSON
   signup: (data: { name: string; username: string; password: string }) => {
-    return API.post("/signup", data); 
+    return API.post("/signup", data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   },
 };
