08f6e047d10d0f7b6f3d02d447466fa80304012c
Dharshini <dharshu@example.com>
2025-10-09 10:53:17 +0530

    service

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,21 +2,16 @@ import API from "./api";
 
 export const AuthService = {
   signin: (data: { username: string; password: string }) => {
-    return API.post(
-      "/login",
-      {
-        username: data.username,
-        password: data.password,
+    const formData = new URLSearchParams();
+    formData.append("username", data.username);
+    formData.append("password", data.password);
+    return API.post("/login", formData, {
+      headers: {
+        "Content-Type": "application/json",
       },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    });
   },
-
   signup: (data: { name: string; username: string; password: string }) => {
-    return API.post("/signup", data);
+    return API.post("/signup", data); 
   },
 };
